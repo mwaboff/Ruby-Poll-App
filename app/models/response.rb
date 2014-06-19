5cba40e4c60f670fe7791e6863ff3ced6eec2720
App Academy Student <student@appacademy.io>
2014-06-19 14:26:57 -0700

    Adds respondent has not already answered question validation

--- app/models/response.rb
+++ app/models/response.rb
@@ -25,4 +25,34 @@ class Response < ActiveRecord::Base
   )
   
   validates :answer_choice_id, :user_id, presence: true
+  validate :respondent_has_not_already_answered_question
+  
+  private
+  def respondent_has_not_already_answered_question
+    unless existing_responses.empty?
+      errors[:user_id] << "already answered this question."
+    end
+  end
+    
+  def existing_responses # for a specific user
+    my_query = <<-SQL
+    SELECT
+      *
+    FROM
+      responses JOIN answer_choices ON answer_choice_id = answer_choices.id 
+    WHERE 
+      responses.user_id = ?
+    AND
+      answer_choices.question_id = 
+        (SELECT
+          question_id
+        FROM
+          answer_choices ac
+        WHERE
+          ac.id = ?)
+    SQL
+    Response.find_by_sql([my_query, user_id, answer_choice_id]);
+  end
+  
 end
+
