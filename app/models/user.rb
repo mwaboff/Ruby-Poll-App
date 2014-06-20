464be0c70e7267d42e2fa7629296604be6003b4f
App Academy Student <student@appacademy.io>
2014-06-19 18:01:54 -0700

    Attempt to implement the ability to determine a user's completed polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,38 @@ class User < ActiveRecord::Base
   class_name: "Response"
   )
   validates :user_name, uniqueness: true, presence: true
+  
+  def qs_per_poll
+    Poll.joins(:questions => :answer_choices)
+    .select("polls.*, COUNT(DISTINCT questions.id) questions_in_poll")
+    .group("polls.id")
+  end
+  
+  def resps
+    Poll.joins(:questions => :answer_choices)
+    .joins("LEFT OUTER JOIN responses on responses.answer_choice_id = answer_choices.id")
+    .select("polls.*, COUNT(responses.id) AS answers")
+    .group("polls.id")
+    .where("responses.user_id" => self.id)
+  end
+  
+  def completed_polls
+    qs = qs_per_poll
+    rs = resps
+    
+    qs.select do |poll|
+      poll_questions = poll.questions_in_poll
+      poll_responses = rs.find{|r_poll| r_poll.id == poll.id}
+      poll.questions_in_poll == (rs.find {|rp| rp.id == poll.id}.answers)
+    end
+  end
+  
+    #
+  # def check_polls
+  #   User.joins(:responses => {:answer_choice => {:question => :poll}})
+  # end
+  #
+  # questions_answered = Question.joins(:answer_choices => :responses)
+  # .group('poll.id')
+  # .where('responses.user_id', id).distinct
 end
